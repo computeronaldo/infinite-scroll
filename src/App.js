6018f795a317e57af71f7c48c148cbed1a983edb
Prajwal Choudhary <prajwalchoudhary14@gmail.com>
2023-07-18 02:22:56 +0530

    refact: modified Example1 component to make use of useRef instead of useCallback to setup intersection observer on last item on page.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import Example1 from "./Example1";
 
 // Implementing infinite scroll is just an amalgamation of
-// intersection observer and infinite scrolling
+// intersection observer and pagination
 function App() {
   return <Example1 />;
 }
